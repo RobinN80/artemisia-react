4fa2e5a0f242642462ae1e2125875ca32c47a6d2
Robin Novak <robin.m.novak@gmail.com>
2020-12-16 09:01:30 -0800

    Moved State items to RenderMovie

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -19,18 +19,18 @@ class Header extends Component{
                     <NavbarBrand href='/'></NavbarBrand>
                     <Nav className="mr-auto" navbar >
                         <NavItem className="nav-item">
-                            <NavLink to='/home'>
+                            <NavLink to='/home' className="nav-link">
                                 <i className= "fa fa-film fa-2x"/>
                                 Home
                             </NavLink>
                         </NavItem>
-                        <NavItem >
-                            <NavLink to='/contact'>
+                        <NavItem className="nav-link">
+                            <NavLink to='/contact' >
                             <i className= "fa fa-envelope fa-2x"/>
                                 Contact Us
                             </NavLink>
                         </NavItem>
-                        <NavItem>
+                        <NavItem className="nav-link">
                             <a href='https://www.google.com/maps/place/124+W+Taylor+St,+Reno,+NV+89509/@39.5163325,-119.8126294,17z/data=!4m5!3m4!1s0x809940b84323e03f:0xc03e614646f385e!8m2!3d39.5163325!4d-119.8104407' target='_blank'>
                             
                             <i className="fa fa-map-marker fa-2x" /> Find Us
--- src/components/moviesComponent.js
+++ src/components/moviesComponent.js
@@ -2,84 +2,133 @@ import React, {Component} from 'react';
 import { Button, Card, CardBody, CardFooter, CardImg, CardHeader, Col, Collapse, Form, FormGroup, Modal, ModalBody, ModalHeader, Row } from 'reactstrap';
 import MOVIES from '../shared/movies';
 
-function RenderMovie(props) {
-        const {title, image, date, description} = props.movie;
-        console.log("Movie Props", props);
-        return(
-            <Row style={{display: 'inline-block', width: 350, margin: 10}}>
-                <Card style={{borderRadius: 45, borderWidth: 'thick'}}>
-                    <CardHeader style={{fontSize: 20}}>{title}</CardHeader>
-                    <CardBody>
-                        <div style= {{paddingBottom: 10, fontSize: 18, backgroundColor: "yellowgreen"}}>{date}</div>
-                        <CardImg src={image} alt='movie image' />
-                        <Collapse isOpen= {props.showInfo}>
-                            <Row style={{display: 'inline-block', width: "500", margin: 10}}>
-                                <Card className= "flex-container">
-                                    <CardBody>{description}</CardBody>    
-                                </Card>
-                                <Button onClick = {props.toggleInfo} color="primary">Show Less</Button>
-                            </Row>
-                        </Collapse>
-                            <Collapse isOpen={!props.showInfo}>
-                                <Button onClick={props.toggleInfo} style={{margin: 10}}>More Info</Button> 
+class RenderMovie extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isTicketModalOpen: false,
+            showInfo: false, 
+            numRegular: 0,
+            numSenStud: 0,
+            numMembers: 0,
+            totalPrice: 0,
+
+        }
+
+        this.toggleInfo = this.toggleInfo.bind(this);
+        this.handleChangeRegular= this.handleChangeRegular.bind(this);
+        this.handleChangeSenStud= this.handleChangeSenStud.bind(this);
+        this.handleChangeMember= this.handleChangeMember.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+    }
+
+    toggleTicketModal = () => {
+        this.setState({isTicketModalOpen: !this.state.isTicketModalOpen});
+    }
+
+    toggleInfo(){
+        this.setState({ showInfo: !this.state.showInfo });
+    }
+
+    
+    handleChangeRegular(event){
+            this.setState({numRegular: event.target.value});
+    }
+    
+    handleChangeSenStud(event){
+            this.setState({numSenStud: event.target.value});
+    }
+    
+    handleChangeMember(event){
+            this.setState({numMembers: event.target.value});
+        }
+    
+    handleSubmit(){
+            console.log('tickets:', this.state.numMembers, this.state.numSenStud, this.state.numRegular);
+            this.setState ({totalPrice : this.state.numRegular*9 + this.state.numSenStud*7 + this.state.numMembers*5});
+            this.toggleTicketModal();
+        } 
+        
+        render(){
+            const {title, image, date, description} = this.props.movie;
+            console.log("Movie Props", this.props);
+            return(
+                <Row style={{display: 'inline-block', width: 350, margin: 10}}>
+                    <Card style={{borderRadius: 45, borderWidth: 'thick'}}>
+                        <CardHeader style={{fontSize: 20}}>{title}</CardHeader>
+                        <CardBody>
+                            <div style= {{paddingBottom: 10, fontSize: 18, backgroundColor: "yellowgreen"}}>{date}</div>
+                            <CardImg src={image} alt='movie image' />
+                            <Collapse isOpen= {this.state.showInfo}>
+                                <Row style={{display: 'inline-block', width: "500", margin: 10}}>
+                                    <Card className= "flex-container">
+                                        <CardBody>{description}</CardBody>    
+                                    </Card>
+                                    <Button onClick = {this.toggleInfo} color="primary">Show Less</Button>
+                                </Row>
                             </Collapse>
-                            <Button onClick={props.toggleTicketModal} style={{margin: 10}} color="primary">Buy Tickets</Button>
-                    </CardBody> 
-                    <Modal isOpen={props.isTicketModalOpen}>
-                    <ModalHeader toggle={props.toggleTicketModal} ><i className= "fa fa-ticket-alt fa-2x"/>Purchase Tickets for {title}</ModalHeader>
-                    <ModalBody>
-                        <Form>
-                            <FormGroup style={{textAlign :'center', fontSize : 'large'}}>
-                                Regular Admission $9 {''}
-                                <select value= {props.numRegular} onChange={props.handleChangeRegular}>
-                                    <option value={0}>0</option>
-                                    <option value={1}>1</option>
-                                    <option value={2}>2</option>
-                                    <option value={3}>3</option>
-                                    <option value={4}>4</option>
-                                    <option value={5}>5</option>
-                                    <option value={6}>6</option>
-                                </select>
-
-                            </FormGroup>
-                            <FormGroup style={{textAlign :'center', fontSize : 'large'}}>
-                                Seniors and Students $7 {''}
-                                <select value={props.numSenStud} onChange={props.handleChangeSenStud}>
-                                    <option value={0}>0</option>
-                                    <option value={1}>1</option>
-                                    <option value={2}>2</option>
-                                    <option value={3}>3</option>
-                                    <option value={4}>4</option>
-                                    <option value={5}>5</option>
-                                    <option value={6}>6</option>
-                                </select>
-                            </FormGroup>
-                            <FormGroup style={{textAlign :'center', fontSize : 'large'}}>
-                                <label>Members $5 {''}
-                                    <select value={props.numMembers} onChange={props.handleChangeMember} >
-                                        <option value={0}>0</option>
-                                        <option value={1}>1</option>
-                                        <option value={2}>2</option>
-                                        <option value={3}>3</option>
-                                        <option value={4}>4</option>
-                                        <option value={5}>5</option>
-                                        <option value={6}>6</option>
-                                    </select>
-                                </label>
-                            </FormGroup>
-                            <FormGroup>
-                            Total Price:  $ {''} {props.numRegular*9 + props.numSenStud*7 + props.numMembers*5 }
-                            <Button onClick={props.handleSubmit}>
-                                Purchase Tickets
-                            </Button>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-
-                </Modal>
-                </Card>
-            </Row>
-        )
+                                <Collapse isOpen={!this.state.showInfo}>
+                                    <Button onClick={this.toggleInfo} style={{margin: 10}}>More Info</Button> 
+                                </Collapse>
+                                <Button onClick={this.toggleTicketModal} style={{margin: 10}} color="primary">Buy Tickets</Button>
+                        </CardBody> 
+                        <Modal isOpen={this.state.isTicketModalOpen}>
+                            <ModalHeader toggle={this.toggleTicketModal} ><i className= "fa fa-ticket-alt fa-2x"/>Purchase Tickets for {title}</ModalHeader>
+                            <ModalBody>
+                                <Form>
+                                    <FormGroup style={{textAlign :'center', fontSize : 'large'}}>
+                                        Regular Admission $9 {''}
+                                        <select value= {this.state.numRegular} onChange={this.handleChangeRegular}>
+                                            <option value={0}>0</option>
+                                            <option value={1}>1</option>
+                                            <option value={2}>2</option>
+                                            <option value={3}>3</option>
+                                            <option value={4}>4</option>
+                                            <option value={5}>5</option>
+                                            <option value={6}>6</option>
+                                        </select>
+        
+                                    </FormGroup>
+                                    <FormGroup style={{textAlign :'center', fontSize : 'large'}}>
+                                        Seniors and Students $7 {''}
+                                        <select value={this.props.numSenStud} onChange={this.handleChangeSenStud}>
+                                            <option value={0}>0</option>
+                                            <option value={1}>1</option>
+                                            <option value={2}>2</option>
+                                            <option value={3}>3</option>
+                                            <option value={4}>4</option>
+                                            <option value={5}>5</option>
+                                            <option value={6}>6</option>
+                                        </select>
+                                    </FormGroup>
+                                    <FormGroup style={{textAlign :'center', fontSize : 'large'}}>
+                                        <label>Members $5 {''}
+                                            <select value={this.state.numMembers} onChange={this.handleChangeMember} >
+                                                <option value={0}>0</option>
+                                                <option value={1}>1</option>
+                                                <option value={2}>2</option>
+                                                <option value={3}>3</option>
+                                                <option value={4}>4</option>
+                                                <option value={5}>5</option>
+                                                <option value={6}>6</option>
+                                            </select>
+                                        </label>
+                                    </FormGroup>
+                                    <FormGroup>
+                                    Total Price:  $ {''} {this.state.numRegular*9 + this.state.numSenStud*7 + this.state.numMembers*5 }
+                                    <Button onClick={this.props.handleSubmit}>
+                                        Purchase Tickets
+                                    </Button>
+                                    </FormGroup>
+                                </Form>
+                            </ModalBody>
+    
+                        </Modal>
+                    </Card>
+                </Row>
+            )
+        }
+        
 
 }
 
@@ -88,75 +137,21 @@ class Movies extends Component{
     constructor(props){
         super(props);
         this.state = {
-            showInfo: false, 
-            isTicketModalOpen: false,
-            numRegular: 0,
-            numSenStud: 0,
-            numMembers: 0,
-            totalPrice: 0,
-
+            movies: MOVIES,
         };
 
-        this.toggleInfo = this.toggleInfo.bind(this);
-        this.toggleTicketModal= this.toggleTicketModal.bind(this);
-        this.handleChangeRegular= this.handleChangeRegular.bind(this);
-        this.handleChangeSenStud= this.handleChangeSenStud.bind(this);
-        this.handleChangeMember= this.handleChangeMember.bind(this);
-        this.handleSubmit= this.handleSubmit.bind(this);
 
     }
-
-
-    toggleInfo(){
-    this.setState({ showInfo: !this.state.showInfo });
-    }
-
-    toggleTicketModal(){
-    this.setState({ isTicketModalOpen : !this.state.isTicketModalOpen});
-    }
-
-    handleChangeRegular(event){
-        this.setState({numRegular: event.target.value});
-    }
-
-    handleChangeSenStud(event){
-        this.setState({numSenStud: event.target.value});
-    }
-
-    handleChangeMember(event){
-        this.setState({numMembers: event.target.value});
-    }
-
-    handleSubmit(){
-        console.log('tickets:', this.state.numMembers, this.state.numSenStud, this.state.numRegular);
-        this.setState ({totalPrice : this.state.numRegular*9 + this.state.numSenStud*7 + this.state.numMembers*5});
-        this.toggleTicketModal();
-    } 
-
-
 
     render(){
     return ( 
             <div>
-            {MOVIES.map( MOVIE => 
+            {this.state.movies.map( MOVIE =>  
                 {
                 return(
                         <RenderMovie 
                             key={MOVIE.id} 
                             movie={MOVIE} 
-                            showInfo = {this.state.showInfo}
-                            toggleInfo={this.toggleInfo}
-                            toggleTicketModal={this.toggleTicketModal}
-                            isTicketModalOpen={this.state.isTicketModalOpen}
-                            value= {this.state.value}
-                            totalPrice={this.handleTicketsPrice}
-                            numMembers={this.state.numMembers}
-                            numRegular={this.state.numRegular}
-                            numSenStud={this.state.numSenStud}
-                            handleChangeMember={this.handleChangeMember}
-                            handleChangeRegular={this.handleChangeRegular}
-                            handleChangeSenStud={this.handleChangeSenStud}
-                            handleSubmit={this.handleSubmit}
                         />
                 );
                 })
