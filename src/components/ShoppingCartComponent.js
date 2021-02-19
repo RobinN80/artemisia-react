1dffda64dc444a92b214a780326774c202e741b6
Robin Novak <robin.m.novak@gmail.com>
2021-02-18 20:02:25 -0800

    shoppingcartitem as child of shopping cart

--- src/components/ShoppingCartComponent.js
+++ src/components/ShoppingCartComponent.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Media , Button } from "reactstrap";
+import { Media, Button } from "reactstrap";
 import { connect } from "react-redux";
-import '../ComponentCSS/ShoppingCart.css';
-import {deleteItemFromCart} from '../redux/actions';
+import "../ComponentCSS/ShoppingCart.css";
+import { deleteItemFromCart } from "../redux/actions";
 import { Component } from "react";
-
 
 const mapStateToProps = (state) => {
   return {
@@ -13,53 +12,65 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = {
-  deleteItemFromCart: (title, image, numRegular, numSenStud, numMembers) => (deleteItemFromCart(title, image, numRegular, numSenStud, numMembers)) 
+  deleteItemFromCart: (title, image, numRegular, numSenStud, numMembers) =>
+    deleteItemFromCart(title, image, numRegular, numSenStud, numMembers),
 };
 
-class ShoppingCart extends Component {
-  constructor(props){
+class ShoppingCartItem extends Component {
+  constructor(props) {
     super(props);
   }
 
   handleDelete = () => {
-    this.props.deleteItemFromCart()
-  }
+    const {ticket} = this.props;
+    this.props.deleteItemFromCart(ticket);
+  };
 
-  render(){
-    const tickets = this.props.shoppingCart.tickets;
-    console.log("shoppingCart:", tickets);
-  
-    if (tickets.length === 0) {
-      return <Media className="empty-cart">Your Shopping Cart is Empty!</Media>;
-    } else {
-      return (
-        <div>
-          {tickets.map((ticket) => {
-            return (
-              <Media className="media-group">
-                <Media left middle className = "media-group">
-                  <Media object className="media-image" src={ticket.image} alt={ticket.title} />
-                </Media>
-                <Media body>
-                  <Media heading>{ticket.title}</Media>
-                  <ul>
-                    <li>Regular Tickets: {ticket.numRegular}</li>
-                    <li>Member Tickets: {ticket.numMembers}</li>
-                    <li>Senior/Student Tickets: {ticket.numSenStud}</li>
-                  </ul>
-                  
-                </Media>
-                <Button onClick = {this.handleDelete}>Remove Tickets</Button>
-              </Media>
-            );
-          })}
-        </div>
-      );
-    }
+  render() {
+    
+    const {ticket} = this.props;
+    return (
+      <Media className="media-group">
+        <Media left middle className="media-group">
+          <Media
+            object
+            className="media-image"
+            src={ticket.image}
+            alt={ticket.title}
+          />
+        </Media>
+        <Media body>
+          <Media heading>{ticket.title}</Media>
+          <ul>
+            <li>Regular Tickets: {ticket.numRegular}</li>
+            <li>Member Tickets: {ticket.numMembers}</li>
+            <li>Senior/Student Tickets: {ticket.numSenStud}</li>
+          </ul>
+        </Media>
+        <Button onClick={this.handleDelete}>Remove Tickets</Button>
+      </Media>
+    );
   }
-  
 }
 
+function ShoppingCart(props) {
+  
+  const tickets = props.shoppingCart.tickets;
+  console.log("shoppingCart:", tickets);
 
+  if (tickets.length === 0) {
+    return <Media className="empty-cart">Your Shopping Cart is Empty!</Media>;
+  } else {
+    return (
+      <div>
+        {tickets.map((ticket) => {
+          return <ConnectedShoppingCartItem key={ticket.title} ticket={ticket} />;
+        })}
+      </div>
+    );
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart);
+const ConnectedShoppingCartItem = connect(null, mapDispatchToProps)(ShoppingCartItem);
+
+export default connect(mapStateToProps)(ShoppingCart);
