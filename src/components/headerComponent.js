beab66d8b9e64bd24cbf4e48eca70203126c32bb
Robin Novak <robin.m.novak@gmail.com>
2021-03-12 15:48:12 -0800

    updated navbar with toggler

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,52 +1,78 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import {
+  Nav,
+  Navbar,
+  NavItem,
+  NavbarBrand,
+  Jumbotron,
+  Collapse,
+  NavbarToggler,
+} from "reactstrap";
 
-import React, {Component} from 'react';
-import { NavLink } from 'react-router-dom';
-import { Nav, Navbar, NavItem, NavbarBrand, Jumbotron,} from 'reactstrap';
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+    };
+  }
 
+  toggleNav = () => this.setState({ isNavOpen: !this.state.isNavOpen });
 
-
-class Header extends Component{
-
-    render(){
-        return(
-            <>
-            <Jumbotron fluid  style={{backgroundColor: '#f77D63', margin: 10, padding: 10, textAlign: 'left'}}>
-                <h1><img src={'../shared/projector.png'}/>Welcome to the Artemisia MovieHouse</h1>
-                <h4>Reno's locally operated movie theater with a difference!</h4>
-            </Jumbotron>
-            <div className="container">
-                <Navbar className="sticky-top" color='light' expand='md'>
-                    <NavbarBrand href='/'></NavbarBrand>
-                    <Nav className="mr-auto" navbar >
-                        <NavItem className="nav-item">
-                            <NavLink to='/home' className="nav-link">
-                                <i className= "fa fa-film fa-2x"/>
-                                Home
-                            </NavLink>
-                        </NavItem>
-                        <NavItem className="nav-link">
-                            <NavLink to='/contact' >
-                            <i className= "fa fa-info-circle fa-2x"/>
-                                About Us
-                            </NavLink>
-                        </NavItem>
-                        <NavItem className="nav-link">
-                            <NavLink to='/findus'>
-                                <i className="fa fa-map-marker fa-2x" /> Find Us
-                            </NavLink>
-                        </NavItem>
-                        <NavItem className="nav-link">
-                            <NavLink to='/cart'>
-                                <i className="fa fa-shopping-cart fa-2x" /> Shopping Cart
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
-                </Navbar>
-            </div>
-        </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Jumbotron
+          fluid
+          style={{
+            backgroundColor: "#f77D63",
+            margin: 10,
+            padding: 10,
+            textAlign: "left",
+          }}
+        >
+          <img src={"../shared/projector.png"} alt="Projector"/>
+          <h1>
+            Welcome to the Artemisia MovieHouse
+          </h1>
+          <h4>Reno's locally operated movie theater with a difference!</h4>
+        </Jumbotron>
+        <Navbar className="sticky-top" color="light" light expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} className="mr-2" />
+            <NavbarBrand href="/">Artemisia</NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem className="nav-link">
+                  <NavLink to="/home">
+                    <i className="fa fa-film fa-2x" />
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem className="nav-link">
+                  <NavLink to="/contact">
+                    <i className="fa fa-info-circle fa-2x" />
+                    About Us
+                  </NavLink>
+                </NavItem>
+                <NavItem className="nav-link">
+                  <NavLink to="/findus">
+                    <i className="fa fa-map-marker fa-2x" /> Find Us
+                  </NavLink>
+                </NavItem>
+                <NavItem className="nav-link">
+                  <NavLink to="/cart">
+                    <i className="fa fa-shopping-cart fa-2x" /> Shopping Cart
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </>
+    );
+  }
 }
-
 
 export default Header;
